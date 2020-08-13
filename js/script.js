ceaac38fdd3b238f24d88b407278c4b2a2e5f2c1
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-08-13 03:40:07 -0300

    add modal

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const score = document.querySelector('.score')
+const paper = document.querySelector('.paper')
+const rock = document.querySelector('.rock')
+const scissors = document.querySelector('.scissors')
+const rules = document.querySelector('.rules')
+
+rules.addEventListener('click', modal)
+
+// function modal {
+
+// }
